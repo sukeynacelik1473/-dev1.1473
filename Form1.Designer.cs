590bab1d9d026b76e33d59317d9a7d4494b61425
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2024-02-13 11:22:30 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ödev1_1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSiralar = new System.Windows.Forms.ListBox();
+            this.btnSiraCikart = new System.Windows.Forms.Button();
+            this.btnSiraAl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbSiralar
+            // 
+            this.lbSiralar.FormattingEnabled = true;
+            this.lbSiralar.Location = new System.Drawing.Point(182, 53);
+            this.lbSiralar.Name = "lbSiralar";
+            this.lbSiralar.Size = new System.Drawing.Size(120, 160);
+            this.lbSiralar.TabIndex = 5;
+            this.lbSiralar.SelectedIndexChanged += new System.EventHandler(this.lbSiralar_SelectedIndexChanged);
+            // 
+            // btnSiraCikart
+            // 
+            this.btnSiraCikart.Location = new System.Drawing.Point(334, 90);
+            this.btnSiraCikart.Name = "btnSiraCikart";
+            this.btnSiraCikart.Size = new System.Drawing.Size(107, 86);
+            this.btnSiraCikart.TabIndex = 4;
+            this.btnSiraCikart.Text = "Sıra Çıkart";
+            this.btnSiraCikart.UseVisualStyleBackColor = true;
+            this.btnSiraCikart.Click += new System.EventHandler(this.btnSiraCikart_Click_1);
+            // 
+            // btnSiraAl
+            // 
+            this.btnSiraAl.Location = new System.Drawing.Point(48, 90);
+            this.btnSiraAl.Name = "btnSiraAl";
+            this.btnSiraAl.Size = new System.Drawing.Size(107, 86);
+            this.btnSiraAl.TabIndex = 3;
+            this.btnSiraAl.Text = "Sıra Al";
+            this.btnSiraAl.UseVisualStyleBackColor = true;
+            this.btnSiraAl.Click += new System.EventHandler(this.btnSiraAl_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSiralar);
+            this.Controls.Add(this.btnSiraCikart);
+            this.Controls.Add(this.btnSiraAl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSiralar;
+        private System.Windows.Forms.Button btnSiraCikart;
+        private System.Windows.Forms.Button btnSiraAl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev1_1473
+{
+    public partial class Form1 : Form
+    {
+        Queue kuyruk = new Queue();
+        int sira = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Listele()
+        {
+            lbSiralar.Items.Clear();
+            foreach (int sira in kuyruk)
+            {
+                lbSiralar.Items.Add(sira);
+            }
+        }
+
+        private void btnSiraCikart_Click(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            Listele();
+        }
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSiraAl_Click_1(object sender, EventArgs e)
+        {
+
+            sira++;
+            kuyruk.Enqueue(sira);
+            Listele();
+        }
+
+        private void lbSiralar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSiraCikart_Click_1(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            Listele();
+        }
+    
+    }
+}
